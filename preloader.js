5ec4e6bbd026164b01722fb1ce4aa2a657d370a8
EJS00102 <52255287+EJS00102@users.noreply.github.com>
2023-11-28 16:44:55 +0200

    Change svg url

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/ejs00102/flutter_preloader/preload.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/EJS00102/flutter_preloader/preload.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
